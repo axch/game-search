0ac134b4585c7bad6b682562b271e6a523e01293
Alexey Radul <axch@mit.edu>
2017-01-21 17:25:14 -0500

    Getting my feet wet defining a basic interface.

---
+++ src/Types.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module Types where
+
+import Data.Random (RVar)
+
+newtype Player = Player Int
+
+class Move a where
+
+class (Eq a, Move m) => Game a m | a -> m where
+    moves :: a -> [m]
+    move  :: m -> a -> (RVar a) -- Random if it's Nature's move; TODO: make the distribution enumerable
+    start :: a
+    payoff :: Player -> a -> Maybe Double -- Nothing if the game isn't over yet
+    current :: a -> Player
+
+-- Decision: A Move is meant to be applicable to many positions (such
+-- as placing a piece in Go), as this seems more common than moves
+-- being bound to the positions they come from.
