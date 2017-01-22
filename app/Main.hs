3c04923bab7226b2509ba83d02ef3501e569bc9f
Alexey Radul <axch@mit.edu>
2017-01-22 08:30:24 -0500

    Draft a driver program to compute win probabilities for random play in TicTacToe.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,26 @@
 module Main where
 
-import Lib
+import Control.Monad
+import Data.Monoid
+
+import Data.Random (RVar)
+
+import MCTS (play_out)
+import TicTacToe
+import Types
+
+payoffs :: TicTacToe -> (Sum Double, Sum Double)
+payoffs g = (Sum p1, Sum p2) where
+    win = payoff g
+    (Just p1) = win $ Player 0
+    (Just p2) = win $ Player 1
+
+
+accumulate :: (Monoid m) => Int -> (TicTacToe -> m) -> TicTacToe -> RVar m
+accumulate n f s = liftM mconcat $ liftM (map f) $ replicateM n (play_out s)
+
+doit :: Int -> TicTacToe -> IO ()
+doit n g = render g >> sampleIO (accumulate n payoffs g) >>= (putStrLn . show)
 
 main :: IO ()
-main = someFunc
+main = undefined
