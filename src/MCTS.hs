acf2fad9afdf7c3c51470fc31bb64c437fb219a7
Alexey Radul <axch@mit.edu>
2017-01-23 11:40:05 -0500

    Draft an implementation of UCT.
    XXX This ignores the possibility of randomness in the effect of a
    move, which is OK for tic-tac-toe but disasterously, (silently?) wrong
    for actually random games.

--- src/MCTS.hs
+++ src/MCTS.hs
@@ -3,6 +3,7 @@ module MCTS where
 import Data.Function (on)
 import Data.List (maximumBy)
 import qualified Data.Map as M
+import Data.Maybe (fromJust)
 
 import Data.Random (RVar)
 import Data.Random.Distribution (rvar)
@@ -22,6 +23,9 @@ play_out strat = go where
        | otherwise = do m <- strat g
                         g' <- move m g
                         go g'
+
+----------------------------------------------------------------------
+-- UCB 1
 
 -- Map each move to the total payoff obtained by going there, together
 -- with the number of times we've gone there
@@ -65,3 +69,64 @@ ucb1_choose tries g = go tries $ empty_level $ moves g where
       m <- select_move level
       level' <- update_once g m level
       go (tries - 1) level'
+
+----------------------------------------------------------------------
+-- UCT
+
+-- The standard UCT tree policy makes an interesting choice, namely to
+-- grow the tree by one node for each play-out.
+
+data UCTree m = UCTree Int (M.Map m (UCTree m, Double, Int))
+
+empty_subtree :: UCTree m
+empty_subtree = UCTree 0 M.empty
+
+-- Choose a move to explore
+-- TODO: they say I ought to break ties randomly, but for now just
+-- taking the lexicographically earliest move.
+select_move' :: UCTree m -> RVar m
+select_move' (UCTree tot state) = return m where
+    value (_, (_, _, 0)) = 1/0
+    value (_, (_, score, tries)) =
+        score / fromIntegral tries
+        + exploration_parameter * sqrt (log (fromIntegral tot) / fromIntegral tries)
+    (m, (_, _, _)) = maximumBy (compare `on` value) $ M.toList state
+
+-- Choose a game state to evaluate with the given (random) evaluation
+-- function, evaluate it, update the tree, and return the evaluation
+-- function for the caller's benefit.
+-- TODO: Since the move's result may be stochastic, I have to know what
+-- Nature chose in order to select the proper sub-tree.
+at_selected_state :: (Game a m, Ord m) => (a -> RVar (Player -> Double)) -> a -> UCTree m
+                     -> RVar ((UCTree m), (Player -> Double))
+at_selected_state eval g t@(UCTree tot state) = do
+  m <- select_move' t
+  g' <- move m g
+  case M.lookup m state of
+    Just (subtree, reward, tries) ->
+        do (subtree', win) <- at_selected_state eval g' subtree
+           let state' = M.insert m (subtree', reward + win (current g), tries + 1) state
+           return (UCTree (tot+1) state', win)
+    Nothing ->
+        do win <- eval g'
+           let state' = M.insert m (empty_subtree, win (current g), 1) state
+           return (UCTree (tot+1) state', win)
+
+one_play_out :: (Game a m) => a -> RVar (Player -> Double)
+one_play_out g = do
+  end <- play_out uniform_choose g
+  return $ fromJust . payoff end
+
+-- Choose a move to return once exploration is done: most explored, in
+-- this case
+select_final_move' :: UCTree m -> RVar m
+select_final_move' (UCTree _ state) = return m where
+    value (_, (_, _, tries)) = tries
+    (m, (_, _, _)) = maximumBy (compare `on` value) $ M.toList state
+
+uct_choose :: (Game a m, Ord m) => Int -> a -> RVar m
+uct_choose tries g = go tries $ empty_subtree where
+    go tries tree | tries == 0 = select_final_move' tree
+                  | otherwise = do
+      (tree', _) <- at_selected_state one_play_out g tree
+      go (tries-1) tree'
