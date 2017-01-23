8c21d37af7ede0fe52bf50c0988baa021d2fa806
Alexey Radul <axch@mit.edu>
2017-01-23 09:22:06 -0500

    Better name.

--- src/Umpire.hs
+++ src/Umpire.hs
@@ -7,8 +7,8 @@ import Data.Random (RVar)
 
 import Types
 
-play_out :: (Game a m) => (a -> RVar m) -> a -> RVar a -- Where the returned state is terminal
-play_out strat = go where
+game :: (Game a m) => (a -> RVar m) -> a -> RVar a -- Where the returned state is terminal
+game strat = go where
   go g | finished g = return g
        | otherwise = do m <- strat g
                         g' <- assert (valid m g) $ move m g
@@ -16,4 +16,4 @@ play_out strat = go where
 
 match :: (Monoid res, Game a m) => Int -> (a -> RVar m) -> (a -> res) -> a -> RVar res
 match n strat eval start =
-    liftM mconcat $ liftM (map eval) $ replicateM n (play_out strat start)
+    liftM mconcat $ liftM (map eval) $ replicateM n (game strat start)
