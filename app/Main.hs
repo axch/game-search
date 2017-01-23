be505947d9c0a07f1932bb13b561e4401eab2578
Alexey Radul <axch@mit.edu>
2017-01-23 13:54:40 -0500

    Covert Main into a benchmark I can execute, is I want to measure effectiveness of optimization.

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,9 @@
 module Main where
 
 import Data.Monoid
+import qualified System.Environment as Sys
 
-import MCTS (uniform_choose, ucb1_choose)
+import MCTS (uniform_choose, ucb1_choose, uct_choose)
 import TicTacToe
 import Types
 import Umpire (match, versus, render_evaluation)
@@ -18,5 +19,11 @@ results g =
 win_probs :: Int -> TicTacToe -> IO ()
 win_probs n g = render_evaluation (match n (versus [ucb1_choose 100, ucb1_choose 1000]) results) g
 
+benchmark :: Int -> Int -> IO ()
+benchmark games budget = render_evaluation (match games strat results) (start :: TicTacToe) where
+    strat = versus [(uct_choose budget), (uct_choose budget)]
+
 main :: IO ()
-main = undefined
+main = do
+  [arg1, arg2] <- Sys.getArgs
+  benchmark (read arg1) (read arg2)
