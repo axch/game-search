436d9d9ecdb76b45f6e9bba2ea0d4af7fd7513ad
Alexey Radul <axch@mit.edu>
2017-01-23 09:25:04 -0500

    I actually want to compute win/lose/draw probabilities, not player payoffs, in this case.

--- app/Main.hs
+++ app/Main.hs
@@ -9,15 +9,16 @@ import TicTacToe
 import Types
 import Umpire (match)
 
-payoffs :: TicTacToe -> (Sum Double, Sum Double)
-payoffs g = (Sum p1, Sum p2) where
-    win = payoff g
-    (Just p1) = win $ Player 0
-    (Just p2) = win $ Player 1
-
+results :: TicTacToe -> (Sum Int, Sum Int, Sum Int)
+results g =
+    case winner g of
+      Right (Player 0) -> (Sum 1, Sum 0, Sum 0)
+      Right (Player 1) -> (Sum 0, Sum 1, Sum 0)
+      Left (Just ()) -> (Sum 0, Sum 0, Sum 1)
+      Left Nothing -> (Sum 0, Sum 0, Sum 0)
 
 win_probs :: Int -> TicTacToe -> IO ()
-win_probs n g = render g >> sampleIO (match n uniform_choose payoffs g) >>= (putStrLn . show)
+win_probs n g = render g >> sampleIO (match n uniform_choose results g) >>= (putStrLn . show)
 
 doit :: (TicTacToe -> RVar TicMove) -> TicTacToe -> IO ()
 doit strat g = do
