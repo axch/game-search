5cc99cea6261b166c384ef702a970a758dda8624
Alexey Radul <axch@mit.edu>
2017-02-09 17:47:56 -0500

    Refactor SDiceWithDeath to be a Position rather than a "Space". Benefit: much better memoization of the transition from Crypt to Dice With Death.

--- src/Expectimax.hs
+++ src/Expectimax.hs
@@ -7,6 +7,7 @@ import Data.IORef
 import Data.List (maximumBy)
 import qualified Data.Map as M
 import Data.Maybe (fromJust)
+import Debug.Trace (traceShow)
 import System.IO.Unsafe
 
 import Types
@@ -15,12 +16,12 @@ import Types
 -- utility from this position, as well as the value thereof, assuming
 -- solitaire and exhaustive search.
 
-best_move :: forall a m. (Ord a, RGame a m) => a -> (Maybe m, Double)
+best_move :: forall a m. (Ord a, Show a, RGame a m) => a -> (Maybe m, Double)
 best_move = answer where
   go g | finished g = (Nothing, fromJust $ payoff g $ Player 0)
        | otherwise = maximumBy (compare `on` snd) $ map evaluate $ moves g
     where
-      evaluate m = (Just m, expectation $ fmap (snd . answer) results)
+      evaluate m = (Just m, expectation $ fmap (snd . answer . (\x -> traceShow x x)) results)
           where
             results :: Probabilities Double a
             results = r_move m g
--- src/Talisman.hs
+++ src/Talisman.hs
@@ -10,7 +10,6 @@ type Die = Int
 data Space = SValleyOfFire
            | SWerewolf
            | SFightWerewolf Int
-           | SDiceWithDeath
            | SCrypt
            | SPlainOfPeril
            | SPortalOfPower
@@ -18,6 +17,7 @@ data Space = SValleyOfFire
 data Position = ValleyOfFire
               | Werewolf Die Die
               | FightWerewolf Int Die Die
+              | SDiceWithDeath
               | DiceWithDeath Die Die Die Die
               | DiceWithDeathA Die Die Die Die -- Rerolled one of mine first
               | DiceWithDeathB Die Die Die Die -- Rerolled one of Death's first
@@ -37,12 +37,6 @@ initial_position SCrypt = do
   d2 <- d6
   d3 <- d6
   return $ Crypt d1 d2 d3
-initial_position SDiceWithDeath = do
-  d1 <- d6
-  d2 <- d6
-  d3 <- d6
-  d4 <- d6
-  return $ DiceWithDeath d1 d2 d3 d4
 initial_position SWerewolf = do
   d1 <- d6
   d2 <- d6
@@ -96,6 +90,7 @@ available_moves :: Position -> [Move]
 available_moves (PortalOfPower _ _) = [Accept, Reroll 0, Reroll 1]
 available_moves PlainOfPeril = [Accept]
 available_moves (Crypt _ _ _) = [Accept, Reroll 0, Reroll 1, Reroll 2]
+available_moves SDiceWithDeath = [Accept]
 available_moves (DiceWithDeath _ _ _ _) = [Accept, Reroll 0, Reroll 1, Reroll 2, Reroll 3]
 available_moves (DiceWithDeathA _ _ _ _) = [Accept, Reroll 0, Reroll 1]
 available_moves (DiceWithDeathB _ _ _ _) = [Accept, Reroll 0, Reroll 1]
@@ -131,7 +126,7 @@ do_move (Reroll 1) (Board n s (PortalOfPower d1 d2)) = do
   do_move Accept (Board n (lose_fate 1 s) (PortalOfPower d1 new_d))
 do_move Accept b@(Board _ _ PlainOfPeril) = move_to SCrypt b
 do_move Accept b@(Board n s (Crypt d1 d2 d3))
-    | strength s >= d1 + d2 + d3 = move_to SDiceWithDeath b
+    | strength s >= d1 + d2 + d3 = return $ Board (n-1) s SDiceWithDeath
     | strength s + 1 == d1 + d2 + d3 = move_to SPlainOfPeril b
     | strength s + 2 == d1 + d2 + d3 = move_to SPortalOfPower b
     | strength s + 3 == d1 + d2 + d3 = move_to SPortalOfPower b
@@ -145,10 +140,16 @@ do_move (Reroll 1) (Board n s (Crypt d1 d2 d3)) = do
 do_move (Reroll 2) (Board n s (Crypt d1 d2 d3)) = do
   new_d <- d6
   do_move Accept (Board n (lose_fate 1 s) (Crypt d1 d2 new_d))
+do_move Accept b@(Board n s SDiceWithDeath) = do
+  d1 <- d6
+  d2 <- d6
+  d3 <- d6
+  d4 <- d6
+  return $ Board n s $ DiceWithDeath d1 d2 d3 d4
 do_move Accept b@(Board n s p@(DiceWithDeath d1 d2 d3 d4))
     | d1 + d2 >  d3 + d4 = move_to SWerewolf b
-    | d1 + d2 == d3 + d4 = move_to SDiceWithDeath b
-    | otherwise = move_to SDiceWithDeath (Board n (lose_life 1 s) p)
+    | d1 + d2 == d3 + d4 = return $ Board (n-1) s SDiceWithDeath
+    | otherwise = return $ Board (n-1) (lose_life 1 s) SDiceWithDeath
 do_move (Reroll 0) (Board n s (DiceWithDeath d1 d2 d3 d4)) = do
   new_d <- d6
   return $ Board n (lose_fate 1 s) $ DiceWithDeathA new_d d2 d3 d4
