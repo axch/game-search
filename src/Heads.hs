fc2aa93374b60681e91594d8ae4ec25ea7aefbde
Alexey Radul <axch@mit.edu>
2017-02-09 15:19:58 -0500

    Write out a variant of Heads with a limited time to win.

--- src/Heads.hs
+++ src/Heads.hs
@@ -27,3 +27,26 @@ instance RGame Heads () where
     payoff Play (Player 0) = Nothing
     payoff Won (Player 0) = Just 1
     current _ = Player 0
+
+-- Here is a variant that gives a specific budget of turns to win, or
+-- you lose.
+
+data TimedHeads = TPlay Int | TWon deriving (Eq, Show)
+
+instance Renderable TimedHeads where
+    render = putStrLn . show
+
+instance RGame TimedHeads () where
+    moves (TPlay _) = [()]
+    moves TWon = []
+    r_move _ (TPlay n) = Probabilities [(1/2, (TPlay $ n-1)), (1/2, TWon)]
+    valid _ (TPlay _) = True
+    valid _ TWon = False
+    start = TPlay 5
+    finished (TPlay 0) = True
+    finished (TPlay _) = False
+    finished TWon = True
+    payoff (TPlay 0) (Player 0) = Just 0
+    payoff (TPlay _) (Player 0) = Nothing
+    payoff TWon (Player 0) = Just 1
+    current _ = Player 0
