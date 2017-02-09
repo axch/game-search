f9cd3524c2af494de7a3f18e6163c19ec990c3cf
Alexey Radul <axch@mit.edu>
2017-02-09 18:48:30 -0500

    Proceed is a better name for the default move, since there are positions from which no rerolls are possible even in principle.

--- src/Talisman.hs
+++ src/Talisman.hs
@@ -55,24 +55,24 @@ instance Renderable Board where
 
 -- The only choices in this model are when to use fate tokens, and
 -- which dice to reroll
-data Move = Accept
+data Move = Proceed
           | Reroll Int -- The index of the die to reroll
   deriving Show
 
 available_moves :: Position -> [Move]
-available_moves SPortalOfPower = [Accept]
-available_moves (PortalOfPower _ _) = [Accept, Reroll 0, Reroll 1]
-available_moves PlainOfPeril = [Accept]
-available_moves SCrypt = [Accept]
-available_moves (Crypt _ _ _) = [Accept, Reroll 0, Reroll 1, Reroll 2]
-available_moves SDiceWithDeath = [Accept]
-available_moves (DiceWithDeath _ _ _ _) = [Accept, Reroll 0, Reroll 1, Reroll 2, Reroll 3]
-available_moves (DiceWithDeathA _ _ _ _) = [Accept, Reroll 0, Reroll 1]
-available_moves (DiceWithDeathB _ _ _ _) = [Accept, Reroll 0, Reroll 1]
-available_moves SWerewolf = [Accept]
-available_moves (Werewolf _ _) = [Accept, Reroll 0, Reroll 1]
-available_moves (SFightWerewolf _) = [Accept]
-available_moves (FightWerewolf _ _ _) = [Accept, Reroll 0]
+available_moves SPortalOfPower = [Proceed]
+available_moves (PortalOfPower _ _) = [Proceed, Reroll 0, Reroll 1]
+available_moves PlainOfPeril = [Proceed]
+available_moves SCrypt = [Proceed]
+available_moves (Crypt _ _ _) = [Proceed, Reroll 0, Reroll 1, Reroll 2]
+available_moves SDiceWithDeath = [Proceed]
+available_moves (DiceWithDeath _ _ _ _) = [Proceed, Reroll 0, Reroll 1, Reroll 2, Reroll 3]
+available_moves (DiceWithDeathA _ _ _ _) = [Proceed, Reroll 0, Reroll 1]
+available_moves (DiceWithDeathB _ _ _ _) = [Proceed, Reroll 0, Reroll 1]
+available_moves SWerewolf = [Proceed]
+available_moves (Werewolf _ _) = [Proceed, Reroll 0, Reroll 1]
+available_moves (SFightWerewolf _) = [Proceed]
+available_moves (FightWerewolf _ _ _) = [Proceed, Reroll 0]
 available_moves ValleyOfFire = []
 
 
@@ -89,26 +89,26 @@ d6 = Probabilities [(p, 1), (p, 2), (p, 3), (p, 4), (p, 5), (p, 6)] where p = 1.
 -- TODO: Define the dice lenses and rewrite all the Reroll cases to
 -- pick a lens and reroll that die.
 do_move :: (Fractional p) => Move -> Board -> Probabilities p Board
-do_move Accept (Board n s SPortalOfPower) = do
+do_move Proceed (Board n s SPortalOfPower) = do
   d1 <- d6
   d2 <- d6
   return $ Board n s $ PortalOfPower d1 d2
-do_move Accept b@(Board n s (PortalOfPower d1 d2))
+do_move Proceed b@(Board n s (PortalOfPower d1 d2))
     | strength s >= d1 + d2 = return $ Board (n-1) s PlainOfPeril
     | otherwise = return $ Board (n-1) (lose_strength 1 s) SPortalOfPower
 do_move (Reroll 0) (Board n s (PortalOfPower d1 d2)) = do
   new_d <- d6
-  do_move Accept (Board n (lose_fate 1 s) (PortalOfPower new_d d2))
+  do_move Proceed (Board n (lose_fate 1 s) (PortalOfPower new_d d2))
 do_move (Reroll 1) (Board n s (PortalOfPower d1 d2)) = do
   new_d <- d6
-  do_move Accept (Board n (lose_fate 1 s) (PortalOfPower d1 new_d))
-do_move Accept (Board n s PlainOfPeril) = return $ Board (n-1) s SCrypt
-do_move Accept (Board n s SCrypt) = do
+  do_move Proceed (Board n (lose_fate 1 s) (PortalOfPower d1 new_d))
+do_move Proceed (Board n s PlainOfPeril) = return $ Board (n-1) s SCrypt
+do_move Proceed (Board n s SCrypt) = do
   d1 <- d6
   d2 <- d6
   d3 <- d6
   return $ Board n s $ Crypt d1 d2 d3
-do_move Accept b@(Board n s (Crypt d1 d2 d3))
+do_move Proceed b@(Board n s (Crypt d1 d2 d3))
     | strength s >= d1 + d2 + d3 = return $ Board (n-1) s SDiceWithDeath
     | strength s + 1 == d1 + d2 + d3 = return $ Board (n-1) s SCrypt
     | strength s + 2 == d1 + d2 + d3 = return $ Board n s SPortalOfPower -- Fencepost on counting steps
@@ -116,20 +116,20 @@ do_move Accept b@(Board n s (Crypt d1 d2 d3))
     | otherwise = return $ Board (n-1) s SPortalOfPower -- TODO Model the outside
 do_move (Reroll 0) (Board n s (Crypt d1 d2 d3)) = do
   new_d <- d6
-  do_move Accept (Board n (lose_fate 1 s) (Crypt new_d d2 d3))
+  do_move Proceed (Board n (lose_fate 1 s) (Crypt new_d d2 d3))
 do_move (Reroll 1) (Board n s (Crypt d1 d2 d3)) = do
   new_d <- d6
-  do_move Accept (Board n (lose_fate 1 s) (Crypt d1 new_d d3))
+  do_move Proceed (Board n (lose_fate 1 s) (Crypt d1 new_d d3))
 do_move (Reroll 2) (Board n s (Crypt d1 d2 d3)) = do
   new_d <- d6
-  do_move Accept (Board n (lose_fate 1 s) (Crypt d1 d2 new_d))
-do_move Accept (Board n s SDiceWithDeath) = do
+  do_move Proceed (Board n (lose_fate 1 s) (Crypt d1 d2 new_d))
+do_move Proceed (Board n s SDiceWithDeath) = do
   d1 <- d6
   d2 <- d6
   d3 <- d6
   d4 <- d6
   return $ Board n s $ DiceWithDeath d1 d2 d3 d4
-do_move Accept (Board n s (DiceWithDeath d1 d2 d3 d4))
+do_move Proceed (Board n s (DiceWithDeath d1 d2 d3 d4))
     | d1 + d2 >  d3 + d4 = return $ Board (n-1) s SWerewolf
     | d1 + d2 == d3 + d4 = return $ Board (n-1) s SDiceWithDeath
     | otherwise = return $ Board (n-1) (lose_life 1 s) SDiceWithDeath
@@ -145,48 +145,48 @@ do_move (Reroll 2) (Board n s (DiceWithDeath d1 d2 d3 d4)) = do
 do_move (Reroll 3) (Board n s (DiceWithDeath d1 d2 d3 d4)) = do
   new_d <- d6
   return $ Board n (lose_fate 1 s) $ DiceWithDeathB d1 d2 d3 new_d
-do_move Accept (Board n s (DiceWithDeathA d1 d2 d3 d4)) =
-    do_move Accept $ Board n s $ DiceWithDeath d1 d2 d3 d4
+do_move Proceed (Board n s (DiceWithDeathA d1 d2 d3 d4)) =
+    do_move Proceed $ Board n s $ DiceWithDeath d1 d2 d3 d4
 do_move (Reroll 0) (Board n s (DiceWithDeathA d1 d2 d3 d4)) = do
   new_d <- d6
-  do_move Accept $ Board n (lose_fate 1 s) $ DiceWithDeath d1 d2 new_d d4
+  do_move Proceed $ Board n (lose_fate 1 s) $ DiceWithDeath d1 d2 new_d d4
 do_move (Reroll 1) (Board n s (DiceWithDeathA d1 d2 d3 d4)) = do
   new_d <- d6
-  do_move Accept $ Board n (lose_fate 1 s) $ DiceWithDeath d1 d2 d3 new_d
-do_move Accept (Board n s (DiceWithDeathB d1 d2 d3 d4)) =
-    do_move Accept $ Board n s $ DiceWithDeath d1 d2 d3 d4
+  do_move Proceed $ Board n (lose_fate 1 s) $ DiceWithDeath d1 d2 d3 new_d
+do_move Proceed (Board n s (DiceWithDeathB d1 d2 d3 d4)) =
+    do_move Proceed $ Board n s $ DiceWithDeath d1 d2 d3 d4
 do_move (Reroll 0) (Board n s (DiceWithDeathB d1 d2 d3 d4)) = do
   new_d <- d6
-  do_move Accept $ Board n (lose_fate 1 s) $ DiceWithDeath new_d d2 d3 d4
+  do_move Proceed $ Board n (lose_fate 1 s) $ DiceWithDeath new_d d2 d3 d4
 do_move (Reroll 1) (Board n s (DiceWithDeathB d1 d2 d3 d4)) = do
   new_d <- d6
-  do_move Accept $ Board n (lose_fate 1 s) $ DiceWithDeath d1 new_d d3 d4
-do_move Accept (Board n s SWerewolf) = do
+  do_move Proceed $ Board n (lose_fate 1 s) $ DiceWithDeath d1 new_d d3 d4
+do_move Proceed (Board n s SWerewolf) = do
   d1 <- d6
   d2 <- d6
   return $ Board n s $ Werewolf d1 d2
-do_move Accept (Board n s (Werewolf d1 d2)) =
+do_move Proceed (Board n s (Werewolf d1 d2)) =
     return $ Board n s $ SFightWerewolf $ d1 + d2
 do_move (Reroll 0) (Board n s (Werewolf d1 d2)) = do
   new_d <- d6
-  do_move Accept $ Board n (lose_fate 1 s) $ Werewolf new_d d2
+  do_move Proceed $ Board n (lose_fate 1 s) $ Werewolf new_d d2
 do_move (Reroll 1) (Board n s (Werewolf d1 d2)) = do
   new_d <- d6
-  do_move Accept $ Board n (lose_fate 1 s) $ Werewolf d1 new_d
-do_move Accept (Board n s (SFightWerewolf str)) = do
+  do_move Proceed $ Board n (lose_fate 1 s) $ Werewolf d1 new_d
+do_move Proceed (Board n s (SFightWerewolf str)) = do
   d1 <- d6
   d2 <- d6
   return $ Board n s $ FightWerewolf str d1 d2
-do_move Accept b@(Board n s (FightWerewolf str d1 d2))
+do_move Proceed b@(Board n s (FightWerewolf str d1 d2))
     | combat_strength s + d1 > str + d2  = return $ Board (n-1) s ValleyOfFire
     | combat_strength s + d1 == str + d2 = return $ Board (n-1) s (SFightWerewolf str)
     | otherwise = return $ Board (n-1) (lose_life 1 s) (SFightWerewolf str)
 do_move (Reroll 0) (Board n s (FightWerewolf str d1 d2)) = do
   new_d <- d6
-  do_move Accept $ Board n (lose_fate 1 s) $ FightWerewolf str new_d d2
+  do_move Proceed $ Board n (lose_fate 1 s) $ FightWerewolf str new_d d2
 
 instance RGame Board Move where
-    moves (Board n Status{..} p) | fate == 0 = [Accept]
+    moves (Board n Status{..} p) | fate == 0 = [Proceed]
                                  | otherwise = available_moves p
     r_move = do_move
     valid m = const True -- TODO: Actually validate the moves
