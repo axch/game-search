a2c0a269793d251e70a54146b9934c106f5d0429
Alexey Radul <axch@mit.edu>
2018-11-18 16:20:59 -0500

    Extend the driver program to iterate over craft values too.

--- app/Main.hs
+++ app/Main.hs
@@ -62,7 +62,7 @@ do_benchmark = do
 
 print_usage_and_exit :: IO ()
 print_usage_and_exit = do
-  putStrLn "Usage: talisman-probabilities max_time_limit max_lives max_fate base_strength max_more_strength combat_bonus"
+  putStrLn "Usage: talisman-probabilities max_time_limit max_lives max_fate base_strength max_more_strength combat_bonus base_craft max_more_craft"
   putStrLn ""
   putStrLn "Computes probabilities of reaching the Valley of Fire in the Talisman endgame"
   putStrLn "- in time turns for time between 6 and max_time_limit"
@@ -70,30 +70,36 @@ print_usage_and_exit = do
   putStrLn "- starting with 0 to max_fate Fate"
   putStrLn "- with base_strength Strength from character sheet, Objects, and Followers"
   putStrLn "- with 0 to max_more_strength additional Strength previously earned"
-  putStrLn "- with combat_bonus additional strength for combat only."
+  putStrLn "- with combat_bonus additional strength for combat only"
+  putStrLn "- with base_craft Craft from character sheet, Objects, and Followers"
+  putStrLn "- with 0 to max_more_craft additional Craft previously earned."
 
 run_talisman :: [String] -> IO ()
 run_talisman args = do
-  let [max_time, max_lives, max_fate, in_strength, max_more, in_bonus] = args
+  let [max_time, max_lives, max_fate, in_strength, max_more_str, in_bonus, in_craft, max_more_cft] = args
   let strength = read in_strength
+      craft = read in_craft
       bonus = read in_bonus
-  putStrLn $ show strength ++ " base strength, " ++ show bonus ++ " combat bonus "
+  putStrLn $ show strength ++ " base strength, " ++ show craft ++ " base craft, " ++ show bonus ++ " combat bonus "
   forM_ [6..(read max_time)]     (\time ->
    forM_ [1..(read max_lives)]    (\lives -> (do
-    putStr "str bon time life fate"
-    forM_ [0..(read max_more)] (\more_strength -> printf " %1d mstr" (more_strength :: Int))
+    putStr "str cft bon time life fate"
+    forM_ [0..(read max_more_str)] (\more_strength -> (do
+     forM_ [0..(read max_more_cft)] (\more_craft -> printf "  +%1d/+%1d" (more_strength :: Int) (more_craft :: Int))))
     putStrLn ""
     forM_ [0..(read max_fate)]     (\fate -> (do
-     printf "%3d %3d %4d %4d %4d" strength bonus time lives fate
-     forM_ [0..(read max_more)] (\more_strength -> (do
-      let (_, value) = best_move $ Tal.Board time (Tal.Status lives fate strength more_strength bonus 0 0) Tal.SPortalOfPower
-      printf " %5.2f%%" $ 100 * value))
+     printf "%3d %3d %3d %4d %4d %4d" strength craft bonus time lives fate
+     forM_ [0..(read max_more_str)] (\more_strength -> (do
+      forM_ [0..(read max_more_cft)] (\more_craft -> (do
+       let status = Tal.Status lives fate strength more_strength bonus craft more_craft
+       let (_, value) = best_move $ Tal.Board time status Tal.SPortalOfPower
+       printf " %5.2f%%" $ 100 * value))))
      putStrLn "")))))
 
 main :: IO ()
 main = do
   args <- Sys.getArgs
-  if length args == 6 then
+  if length args == 8 then
       run_talisman args
   else
       print_usage_and_exit
